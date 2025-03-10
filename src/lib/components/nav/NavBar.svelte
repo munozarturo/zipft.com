426e2e71d21b1b5f2e0d0df2e73e169dfe96cffa
munozarturo <munoz.arturoroman@gmail.com>
2025-03-10 17:40:00 -0400

    build: add navlink styling

--- src/lib/components/nav/NavBar.svelte
+++ src/lib/components/nav/NavBar.svelte
@@ -1,8 +1,22 @@
-<script>
+<script lang="ts">
 	import LogoCore from '$lib/assets/brand/LogoCore.ts.svelte';
 	import Account from '$lib/assets/icons/Account.svelte';
+	import { page } from '$app/state';
+	import { twMerge } from 'tailwind-merge';
 
 	let {} = $props();
+
+	function isActive(path: string | string[], children: boolean = false): boolean {
+		const checkActive = (path: string): boolean => {
+			return page.url.pathname === path || (children && page.url.pathname.startsWith(path));
+		};
+
+		if (typeof path === 'string') {
+			return checkActive(path);
+		} else {
+			return path.some((p) => checkActive(p));
+		}
+	}
 </script>
 
 <nav class="flex flex-row items-center justify-between w-full h-20 px-10">
@@ -14,10 +28,24 @@
 		<div class="flex flex-row items-center">
 			<ul class="flex flex-row items-center">
 				<li>
-					<a href="/" class="text-lg p-2 hover:underline underline-offset-2 decoration-2"> send </a>
+					<a
+						href="/"
+						class={twMerge(
+							'text-lg p-1.5 hover:underline underline-offset-2 decoration-2',
+							isActive('/') && 'underline'
+						)}
+					>
+						send
+					</a>
 				</li>
 				<li>
-					<a href="/receive" class="text-lg p-2 hover:underline underline-offset-2 decoration-2">
+					<a
+						href="/receive"
+						class={twMerge(
+							'text-lg p-2 hover:underline underline-offset-2 decoration-2',
+							isActive(['/receive', '/r'], true) && 'underline'
+						)}
+					>
 						receive
 					</a>
 				</li>
