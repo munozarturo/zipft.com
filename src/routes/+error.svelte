83c1b97e38603f17b19b73f4b9aa9495332f6f7f
munozarturo <munoz.arturoroman@gmail.com>
2025-03-10 17:11:16 -0400

    build: error page mobile

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,16 +8,16 @@
 
 <div>
 	<NavBar />
-	<div class="w-full flex flex-row items-center justify-center mt-40">
-		<div class="flex flex-col w-fit h-fit gap-8">
+	<div class="w-full flex flex-row items-center justify-center px-4 mt-20 md:mt-40">
+		<div class="flex flex-col w-full max-w-md h-fit gap-6 md:gap-8 text-center">
 			<div class="flex flex-col gap-2">
-				<h1 class="font-bold text-5xl">404 Not Found</h1>
-				<h2 class="text-3xl">We couldn't find the page you are looking for.</h2>
+				<h1 class="font-bold text-3xl md:text-5xl">404 Not Found</h1>
+				<h2 class="text-xl md:text-3xl">We couldn't find the page you are looking for.</h2>
 			</div>
 			<div class="flex flex-row items-center justify-center">
 				<a
 					href="/"
-					class="p-2 text-xl transition-all outline w-fit hover:text-secondary hover:bg-primary-800"
+					class="p-2 text-lg md:text-xl transition-all outline w-fit hover:text-secondary hover:bg-primary-800"
 					>Go home</a
 				>
 			</div>
