4f330f56897a02515386fdaa5586aff964e04716
munozarturo <munoz.arturoroman@gmail.com>
2025-03-10 15:10:10 -0400

    build: 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,18 @@
 <script>
-	import { page } from '$app/state';
+	import ChevronRight from '$lib/assets/icons/ChevronRight.svelte';
 	import NavBar from '$lib/components/nav/NavBar.svelte';
 </script>
 
 <svelte:head>
-	<title>404 Error :: zipft</title>
+	<title>404 :: zipft</title>
 </svelte:head>
 
 <div>
 	<NavBar />
-	<div>App Error: 404 not found '{page.url.pathname}'</div>
+	<div class="w-full flex flex-row items-center justify-center mt-40">
+		<div class="flex flex-col w-fit h-fit gap-4">
+			<h1 class="font-bold text-5xl">404 Not Found</h1>
+			<h2 class="text-3xl">We couldn't find the page you were looking for.</h2>
+		</div>
+	</div>
 </div>
