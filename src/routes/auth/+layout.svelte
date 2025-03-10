4e7f1c7b94ed7b771a47059f8500deeffe994ea3
munozarturo <munoz.arturoroman@gmail.com>
2025-03-10 13:43:51 -0400

    build: add auth layout

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import LogoCore from '$lib/assets/brand/LogoCore.ts.svelte';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex flex-col items-center justify-center">
+	<nav class="flex flex-row w-full h-20">
+		<a href="/"><LogoCore className="h-3/4 fm:h-full p-2 ml-4" /></a>
+	</nav>
+	{@render children()}
+</div>
