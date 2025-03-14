89a4cf0d65b8bff13727410490b22b0b0e4615af
munozarturo <munoz.arturoroman@gmail.com>
2025-03-14 09:01:20 -0400

    build: update email source

--- src/routes/(auth)/verify/+page.server.ts
+++ src/routes/(auth)/verify/+page.server.ts
@@ -18,57 +18,66 @@ export const load: PageServerLoad = async (event) => {
 	const email = event.url.searchParams.get('e');
 	if (!email) return redirect(302, `/signin?r=${redirectUrl}`);
 
-	// if user doesn't exist signup
-	const user = await getUserByEmail(email);
-	if (!user) return redirect(302, `/signup?r=${redirectUrl}`);
+	try {
+		// if user doesn't exist signup
+		const user = await getUserByEmail(email);
+		if (!user) return redirect(302, `/signup?r=${redirectUrl}`);
 
-	// if account already verified
-	if (user.verified)
+		// if account already verified
+		if (user.verified)
+			return {
+				redirectUrl,
+				error: false,
+				verified: true,
+				message: 'Account has already been verified.'
+			};
+
+		// check most recent email sent
+		const mostRecentCommunication = await getLastCommunication(user.id, 'email', 'verification');
+		if (mostRecentCommunication) {
+			const lastCommunicationTime = new Date(mostRecentCommunication.createdAt).getTime();
+			const oneMinuteAgo = Date.now() - 60 * 1000;
+
+			if (lastCommunicationTime > oneMinuteAgo) {
+				const waitTime = lastCommunicationTime - oneMinuteAgo;
+				return {
+					redirectUrl,
+					error: true,
+					verified: false,
+					message: 'Please wait before requesting another verification email.',
+					wait: Math.ceil(waitTime / 1000)
+				};
+			}
+		}
+
+		// generate challenge
+		const token = generateToken();
+		await createVerificationChallenge(token, user.id);
+		const communication = await createCommunication(user.id, 'email', 'verification');
+
+		await sendEmail({
+			source: `zipft <auth@account.${ROOT_DOMAIN}>`,
+			destination: { to: email },
+			subject: 'Verify your account',
+			body: {
+				text: `https://${ROOT_DOMAIN}/verify/challenge?t=${token}&r=${redirectUrl}::${communication.id}`,
+				html: `<p>https://${ROOT_DOMAIN}/verify/challenge?t=${token}&r=${redirectUrl}</p><br><p>${communication.id}</p>`
+			}
+		});
+
 		return {
 			redirectUrl,
 			error: false,
-			verified: true,
-			message: 'Account has already been verified.'
+			verified: false,
+			message:
+				'We sent a verification link to your email. Please check your inbox and click the link to complete your account setup.'
 		};
-
-	// check most recent email sent
-	const mostRecentCommunication = await getLastCommunication(user.id, 'email', 'verification');
-	if (mostRecentCommunication) {
-		const lastCommunicationTime = new Date(mostRecentCommunication.createdAt).getTime();
-		const oneMinuteAgo = Date.now() - 60 * 1000;
-
-		if (lastCommunicationTime > oneMinuteAgo) {
-			const waitTime = lastCommunicationTime - oneMinuteAgo;
-			return {
-				redirectUrl,
-				error: true,
-				verified: false,
-				message: 'Please wait before requesting another verification email.',
-				wait: Math.ceil(waitTime / 1000)
-			};
-		}
+	} catch (e: any) {
+		return {
+			redirectUrl,
+			error: false,
+			verified: false,
+			message: e.message || 'An unknown error occured while trying to create a verification link.'
+		};
 	}
-
-	// generate challenge
-	const token = generateToken();
-	await createVerificationChallenge(token, user.id);
-	const communication = await createCommunication(user.id, 'email', 'verification');
-
-	await sendEmail({
-		source: `zipft Security <security@auth.${ROOT_DOMAIN}>`,
-		destination: { to: email },
-		subject: 'Account Verification Token',
-		body: {
-			text: `https://${ROOT_DOMAIN}/verify/challenge?t=${token}&r=${redirectUrl}::${communication.id}`,
-			html: `<p>https://${ROOT_DOMAIN}/verify/challenge?t=${token}&r=${redirectUrl}</p><br><p>${communication.id}</p>`
-		}
-	});
-
-	return {
-		redirectUrl,
-		error: false,
-		verified: false,
-		message:
-			'We sent a verification link to your email. Please check your inbox and click the link to complete your account setup.'
-	};
 };
