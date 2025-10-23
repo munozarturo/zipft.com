5e730e5249faf6af8f99155b22c7ccd451720ac9
Arturo Munoz <munoz.arturoroman@gmail.com>
2025-10-23 13:11:57 -0400

    build: add navbar

--- app/layouts/content.vue
+++ app/layouts/content.vue
@@ -3,51 +3,68 @@
         <template #title> <LogoLong class="h-12 xs:h-14" /> </template>
 
         <template #right>
-            <div
-                v-if="!$auth.isAuthenticated.value"
-                class="flex flex-row gap-2 items-center"
-            >
-                <UColorModeButton />
-                <UButton
+            <div class="flex flex-row items-center gap-4">
+                <UNavigationMenu
+                    :items="items"
                     color="neutral"
-                    trailing-icon="i-lucide-arrow-right"
-                    class="group cursor-pointer hidden xs:flex"
-                    @click="navigateTo('/auth/signin')"
-                    :ui="{
-                        trailingIcon:
-                            'group-enabled:group-hover:translate-x-1 group-enabled:group-focus-visible:translate-x-1 transition-transform duration-300',
-                    }"
+                    class="hidden xs:flex"
+                />
+                <div
+                    v-if="!$auth.isAuthenticated.value"
+                    class="flex flex-row gap-2 items-center"
                 >
-                    <span class="text-left w-full">Sign in</span>
-                </UButton>
-            </div>
-            <div v-else>
-                <AuthDropdown />
+                    <UColorModeButton />
+                    <UButton
+                        color="neutral"
+                        trailing-icon="i-lucide-arrow-right"
+                        class="group cursor-pointer hidden xs:flex"
+                        @click="navigateTo('/auth/signin')"
+                        :ui="{
+                            trailingIcon:
+                                'group-enabled:group-hover:translate-x-1 group-enabled:group-focus-visible:translate-x-1 transition-transform duration-300',
+                        }"
+                    >
+                        <span class="text-left w-full">Sign in</span>
+                    </UButton>
+                </div>
+                <div v-else>
+                    <AuthDropdown />
+                </div>
             </div>
         </template>
 
         <template #body>
-            <div
-                v-if="!$auth.isAuthenticated.value"
-                class="flex xs:hidden flex-row gap-3 px-4 py-3"
-            >
-                <UButton
+            <div class="flex flex-col gap-2">
+                <UNavigationMenu
+                    :items="items"
                     color="neutral"
-                    variant="outline"
-                    icon="i-lucide-log-in"
-                    class="w-full"
-                    @click="navigateTo('/auth/signin')"
+                    orientation="vertical"
+                    class="-mx-2.5"
+                />
+                <div
+                    v-if="!$auth.isAuthenticated.value"
+                    class="flex xs:hidden flex-row gap-3 px-4 py-3"
                 >
-                    Sign in
-                </UButton>
-                <UButton
-                    color="neutral"
-                    trailing-icon="i-lucide-arrow-right"
-                    class="w-full justify-between"
-                    @click="navigateTo('/auth/signup')"
-                >
-                    Sign up
-                </UButton>
+                    <UButton
+                        color="neutral"
+                        variant="outline"
+                        icon="i-lucide-log-in"
+                        class="w-full"
+                        @click="navigateTo('/auth/signin')"
+                        size="sm"
+                    >
+                        Sign in
+                    </UButton>
+                    <UButton
+                        color="neutral"
+                        trailing-icon="i-lucide-arrow-right"
+                        class="w-full justify-between"
+                        @click="navigateTo('/auth/signup')"
+                        size="sm"
+                    >
+                        Sign up
+                    </UButton>
+                </div>
             </div>
         </template>
     </UHeader>
@@ -76,4 +93,23 @@
     </UFooter>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const route = useRoute();
+
+const items = computed<NavigationMenuItem[]>(() => [
+    {
+        label: "Send",
+        to: "/send",
+        icon: "i-lucide-send",
+        active: route.path === "/send",
+    },
+    {
+        label: "Receive",
+        to: "/receive",
+        icon: "i-lucide-inbox",
+        active: route.path === "/receive" || route.path.startsWith("/r/"),
+    },
+]);
+</script>
