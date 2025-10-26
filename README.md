e898894df56000ee17e743be257472f3f3f562ac
Arturo Munoz <munoz.arturoroman@gmail.com>
2025-10-26 13:49:32 -0400

    build: /receive

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Sign in, no account move to sign up.
 emails not importing in vercel build
 
 lowercase email on signup and signin, etc.??? is this safe
+
+Add max transfer size to /send, and add lambda functions to check s3 uploads
+
+Build /receive
+
+Build /r
--- app/pages/receive/index.vue
+++ app/pages/receive/index.vue
@@ -1,7 +1,68 @@
 <template>
-    <NuxtLayout name="content"> </NuxtLayout>
+    <NuxtLayout name="content">
+        <div class="w-full h-full flex flex-col items-center pt-2 pb-4">
+            <div class="w-full px-4 sm:px-0 sm:max-w-lg lg:max-w-2xl">
+                <UForm
+                    class="flex flex-col gap-2 pt-2"
+                    :schema="schema"
+                    :state="state"
+                    @submit="onSubmit"
+                >
+                    <UFormField
+                        name="token"
+                        label="Transfer token"
+                        description="Find a transfer"
+                    >
+                        <UPinInput
+                            color="neutral"
+                            v-model="state.token"
+                            size="xl"
+                            type="text"
+                            :length="6"
+                        />
+                    </UFormField>
+                    <UButton
+                        type="submit"
+                        color="neutral"
+                        trailing-icon="i-lucide-chevron-right"
+                        class="group cursor-pointer w-fit"
+                        :ui="{
+                            trailingIcon:
+                                'group-enabled:group-hover:translate-x-1 group-enabled:group-focus-visible:translate-x-1 transition-transform duration-300',
+                        }"
+                    >
+                        <span class="text-left w-full">Find</span>
+                    </UButton>
+                </UForm>
+            </div>
+        </div>
+    </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+import * as y from "yup";
+import type { FormSubmitEvent } from "@nuxt/ui";
+
 useHead({ title: "Receive :: zipft" });
+
+const schema = y.object({
+    token: y
+        .array(y.string().length(1))
+        .test(
+            "valid-characters",
+            "Token contains invalid characters",
+            (value) => {
+                if (!value) return false;
+                const joined = value.join("");
+                return /^[abcdefghijkmnpqrstuvwxyz23456789]{6}$/.test(joined);
+            }
+        ),
+});
+type Schema = y.InferType<typeof schema>;
+
+const state = reactive({
+    token: Array(6).fill("") as string[],
+});
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {}
 </script>
